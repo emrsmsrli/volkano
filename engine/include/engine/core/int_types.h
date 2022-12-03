f0682ff537b7e7dee0140d673bf535d1850aba5f
emrsmsrli <emrsmsrli1@gmail.com>
2022-12-03 10:15:48 +0300

    move types.h to volkano/int_types.h

--- engine/include/engine/core/int_types.h
+++ engine/include/engine/core/int_types.h
@@ -1,14 +1,11 @@
 /*
- * Copyright (C) 2021 Emre Simsirli
+ * Copyright (C) 2022 Emre Simsirli
  *
  * Licensed under GPLv3 or any later version.
  * Refer to the included LICENSE file.
  */
 
 #pragma once
-
-#ifndef VOLKANO_TYPES_H
-#define VOLKANO_TYPES_H
 
 #include <cstdint>
 #include <cstddef>
@@ -25,10 +22,10 @@ using i64 = std::int64_t;
 using u64 = std::uint64_t;
 using usize = std::size_t;
 using ssize = std::ptrdiff_t;
+using ptrdiff = std::ptrdiff_t;
+using uintptr = std::uintptr_t;
 
 using f32 = float;
 using f64 = double;
 
 } // namespace volkano
-
-#endif // VOLKANO_TYPES_H
