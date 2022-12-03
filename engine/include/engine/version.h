7fc75f13f1fa82f22df6af9c334b79cae37fb0de
emrsmsrli <emrsmsrli1@gmail.com>
2022-12-03 10:13:55 +0300

    add volkano_version.in to autogenerate versioning from cmake

---
+++ engine/include/engine/version.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (C) 2022 Emre Simsirli
+ *
+ * Licensed under GPLv3 or any later version.
+ * Refer to the included LICENSE file.
+ */
+
+#pragma once
+
+#include <string_view>
+
+#include "engine/core/int_types.h"
+
+namespace volkano {
+
+[[maybe_unused]] constexpr u8 version_major = 0;
+[[maybe_unused]] constexpr u8 version_minor = 0;
+[[maybe_unused]] constexpr u8 version_patch = 1;
+
+[[maybe_unused]] constexpr u8 version_int = version_major << 16 | version_minor << 8 | version_patch;
+
+[[maybe_unused]] constexpr std::string_view version = "0.0.1";
+
+} // namespace volkano
