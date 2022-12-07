6e934602e9a6cacfb5d561a429c6c8e84e007ce6
emrsmsrli <emrsmsrli1@gmail.com>
2022-12-07 12:53:14 +0300

    improvements to static_vector

--- engine/include/engine/core/container/static_vector.h
+++ engine/include/engine/core/container/static_vector.h
@@ -7,8 +7,9 @@
 
 #pragma once
 
-#include <array>
+#include <concepts>
 #include <type_traits>
+#include <initializer_list>
 #include <new>
 
 #include "engine/core/int_types.h"
@@ -22,9 +23,7 @@ template<typename T, usize Capacity>
 class static_vector {
     static_assert(Capacity != 0, "Vector capacity must not be zero");
 
-    using storage_t = std::array<aligned_union<T>, Capacity>;
-
-    storage_t storage_;
+    aligned_union<T> storage_[Capacity];
     usize size_ = 0;
 
 public:
@@ -34,93 +33,111 @@ public:
     using const_reference = const value_type&;
     using pointer = T*;
     using const_pointer = const T*;
-    using iterator = typename storage_t::iterator;
-    using const_iterator = typename storage_t::const_iterator;
+    using iterator = typename std::array<T, Capacity>::iterator;
+    using const_iterator = typename std::array<T, Capacity>::const_iterator;
+    using reverse_iterator = typename std::array<T, Capacity>::reverse_iterator;
+    using const_reverse_iterator = typename std::array<T, Capacity>::const_reverse_iterator;
 
-    static_vector() noexcept = default;
-    explicit static_vector(const usize size)
+    constexpr static_vector() noexcept = default;
+    constexpr explicit static_vector(const usize size)
         noexcept(std::is_nothrow_default_constructible_v<T>)
-        requires std::is_default_constructible_v<T>
+        requires std::default_initializable<T>
       : size_(size)
     {
         std::uninitialized_default_construct(begin(), end());
     }
 
-    static_vector(const usize size, const T& value) noexcept(std::is_nothrow_copy_constructible_v<T>)
+    constexpr static_vector(const usize size, const T& value)
+        noexcept(std::is_nothrow_copy_constructible_v<T>)
+        requires std::copy_constructible<T>
       : size_(size)
     {
         std::uninitialized_fill(begin(), end(), value);
     }
 
-    ~static_vector() noexcept(noexcept(destroy_range(begin(), end()))) { destroy_range(begin(), end()); }
+    constexpr ~static_vector() noexcept(std::is_nothrow_destructible_v<T>) { std::destroy(begin(), end()); }
 
-    static_vector(const static_vector&) noexcept(std::is_nothrow_copy_constructible_v<T>) = default;
-    static_vector& operator=(const static_vector&) noexcept(std::is_nothrow_copy_constructible_v<T>) = default;
+    constexpr static_vector(const static_vector&) noexcept(std::is_nothrow_copy_constructible_v<T>) = default;
+    constexpr static_vector& operator=(const static_vector&) noexcept(std::is_nothrow_copy_assignable_v<T>) = default;
 
     template<usize OtherCapacity>
         requires (Capacity >= OtherCapacity)
-    static_vector(static_vector<T, OtherCapacity>&& other) noexcept(std::is_nothrow_move_constructible_v<T>)
+    constexpr static_vector(static_vector<T, OtherCapacity>&& other) noexcept(std::is_nothrow_move_constructible_v<T>) // NOLINT(google-explicit-constructor)
         : size_(std::exchange(other.size_, 0))
     {
-        std::uninitialized_move(other.begin(), other.end(), begin());
+        std::uninitialized_move(other.begin(), other.begin() + size_, begin());
     }
 
     template<usize OtherCapacity>
         requires (Capacity >= OtherCapacity)
-    static_vector& operator=(static_vector<T, OtherCapacity>&& other) noexcept(std::is_nothrow_move_constructible_v<T>)
+    constexpr static_vector& operator=(static_vector<T, OtherCapacity>&& other) noexcept(std::is_nothrow_move_assignable_v<T>)
     {
         size_ = std::exchange(other.size_, 0);
-        std::uninitialized_move(other.begin(), other.end(), begin());
+        std::uninitialized_move(other.begin(), other.begin() + size_, begin());
     }
 
-    [[nodiscard]] T& operator[](const usize idx) noexcept { return *ptr(idx); }
-    [[nodiscard]] const T& operator[](const usize idx) const noexcept { return *ptr(idx); }
-    [[nodiscard]] T* ptr(const usize idx) noexcept { return std::launder(reinterpret_cast<T*>(storage_.ptr(idx))); } // NOLINT
-    [[nodiscard]] const T* ptr(const usize idx) const noexcept { return std::launder(reinterpret_cast<const T*>(storage_.ptr(idx))); } // NOLINT
-    [[nodiscard]] T& at(const usize idx) noexcept { VK_ASSERT(idx < size_); return *ptr(idx); }
-    [[nodiscard]] const T& at(const usize idx) const noexcept { VK_ASSERT(idx < size_); return *ptr(idx); }
-    [[nodiscard]] T* data() noexcept { return ptr(0); }
-    [[nodiscard]] const T* data() const noexcept { return ptr(0); }
+    constexpr static_vector(std::initializer_list<T> elems) noexcept(std::is_nothrow_copy_constructible_v<T>)
+      : size_(elems.size())
+    {
+        VK_ASSERT(size_ <= Capacity);
+        std::uninitialized_copy(elems.begin(), elems.end(), begin());
+    };
+
+    constexpr static_vector& operator=(std::initializer_list<T> elems) noexcept(std::is_nothrow_copy_assignable_v<T>)
+    {
+        VK_ASSERT(elems.size() <= Capacity);
+        size_ = elems.size();
+        std::uninitialized_copy(elems.begin(), elems.end(), begin());
+    }
+
+    [[nodiscard]] constexpr T& operator[](const usize idx) noexcept { return *ptr(idx); }
+    [[nodiscard]] constexpr const T& operator[](const usize idx) const noexcept { return *ptr(idx); }
+    [[nodiscard]] constexpr T* ptr(const usize idx) noexcept { return storage_[idx].template value<T>(); }
+    [[nodiscard]] constexpr const T* ptr(const usize idx) const noexcept { return storage_[idx].template value<T>(); }
+    [[nodiscard]] constexpr T& at(const usize idx) noexcept { VK_ASSERT(idx < size_); return *ptr(idx); }
+    [[nodiscard]] constexpr const T& at(const usize idx) const noexcept { VK_ASSERT(idx < size_); return *ptr(idx); }
+    [[nodiscard]] constexpr T* data() noexcept { return ptr(0); }
+    [[nodiscard]] constexpr const T* data() const noexcept { return ptr(0); }
 
     template<typename... Args>
-        requires (std::is_constructible_v<T, Args...>)
+        requires std::constructible_from<T, Args...>
     constexpr T& emplace_back(Args&&... args) noexcept(std::is_nothrow_constructible_v<T, Args...>)
     {
-        static_assert(std::is_constructible_v<T, Args...>);
         VK_ASSERT(size_ < Capacity);
-        std::construct_at(&storage_[size_], std::forward<Args>(args)...);
+        storage_[size_].template construct<T>(std::forward<Args>(args)...);
         return *ptr(size_++);
     }
 
     constexpr void push_back(const T& t) noexcept(std::is_nothrow_copy_constructible_v<T>)
     {
         VK_ASSERT(size_ < Capacity);
-        std::construct_at(ptr(size_++), t);
+        storage_[size_++].template construct<T>(t);
     }
 
     constexpr void push_back(T&& t) noexcept(std::is_nothrow_move_constructible_v<T>)
     {
         VK_ASSERT(size_ < Capacity);
-        std::construct_at(ptr(size_++), std::move(t));
+        storage_[size_++].template construct<T>(std::move(t));
     }
 
     [[nodiscard]] constexpr bool empty() const noexcept { return size_ == 0; }
     [[nodiscard]] constexpr usize size() const noexcept { return size_; }
+    [[nodiscard]] constexpr usize max_size() const noexcept { return Capacity; }
     [[nodiscard]] constexpr usize capacity() const noexcept { return Capacity; }
 
-    T& front() noexcept { return at(0); }
-    const T& front() const noexcept { return at(0); }
-    T& back() noexcept { return at(size() - 1); }
-    const T& back() const noexcept { return at(size() - 1); }
+    constexpr T& front() noexcept { return at(0); }
+    constexpr const T& front() const noexcept { return at(0); }
+    constexpr T& back() noexcept { return at(size() - 1); }
+    constexpr const T& back() const noexcept { return at(size() - 1); }
 
-    void erase(iterator first, iterator last) noexcept
+    constexpr void erase(iterator first, iterator last) noexcept
     {
         std::destroy(first, last);
         std::rotate(first, last, end());
         size_ -= static_cast<usize>(std::distance(first, last));
     }
 
-    void erase(iterator iter) noexcept
+    constexpr void erase(iterator iter) noexcept
     {
         erase(iter, iter + 1);
     }
@@ -137,12 +154,33 @@ public:
         size_ = 0;
     }
 
-    constexpr iterator begin() noexcept { return iterator{0}; }
-    constexpr iterator end() noexcept { return iterator{size_}; }
-    constexpr const_iterator begin() const noexcept { return const_iterator{0}; }
-    constexpr const_iterator end() const noexcept { return const_iterator{size_}; }
-    constexpr const_iterator cbegin() const noexcept { return const_iterator{0}; }
-    constexpr const_iterator cend() const noexcept { return const_iterator{size_}; }
+    constexpr iterator begin() noexcept { return iterator{ptr(0)}; }
+    constexpr const_iterator begin() const noexcept { return const_iterator{ptr(0)}; }
+    constexpr iterator end() noexcept { return iterator{ptr(0), size_}; }
+    constexpr const_iterator end() const noexcept { return const_iterator{ptr(0), size_}; }
+    constexpr reverse_iterator rbegin() noexcept { return reverse_iterator{end()}; }
+    constexpr const_reverse_iterator rbegin() const noexcept { return const_reverse_iterator{begin()}; }
+    constexpr reverse_iterator rend() noexcept { return reverse_iterator{end()}; }
+    constexpr const_reverse_iterator rend() const noexcept { return const_reverse_iterator{begin()}; }
+    constexpr const_iterator cbegin() const noexcept { return const_iterator{ptr(0)}; }
+    constexpr const_iterator cend() const noexcept { return const_iterator{ptr(0), size_}; }
+    constexpr const_reverse_iterator crbegin() const noexcept { return const_reverse_iterator{cend()}; }
+    constexpr const_reverse_iterator crend() const noexcept { return const_reverse_iterator{cbegin()}; }
+
+    constexpr void swap(static_vector& other) noexcept(std::is_nothrow_swappable_v<T>)
+    {
+        using std::swap;
+        swap(storage_, other.storage_);
+        swap(size_, other.size_);
+    }
 };
+
+// todo implement equality/comparison operators
+
+template<typename T, usize Capacity>
+constexpr void swap(static_vector<T, Capacity>& left, static_vector<T, Capacity>& right) noexcept(std::is_nothrow_swappable_v<T>)
+{
+    left.swap(right);
+}
 
 } // namespace volkano
