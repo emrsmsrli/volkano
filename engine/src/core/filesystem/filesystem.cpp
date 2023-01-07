39fb5db60b9d330305661838d814942539d9b806
emrsmsrli <emrsmsrli1@gmail.com>
2023-01-07 11:37:05 +0300

    modernize fs::read and fs::write

--- engine/src/core/filesystem/filesystem.cpp
+++ engine/src/core/filesystem/filesystem.cpp
@@ -5,7 +5,10 @@
  * Refer to the included LICENSE file.
  */
 
+#include <algorithm>
 #include <fstream>
+#include <iterator>
+#include <ranges>
 
 #include "engine/core/filesystem/filesystem.h"
 #include "engine/core/assert.h"
@@ -16,28 +19,33 @@ namespace volkano::fs {
 
 std::vector<u8> read_file(const path& path)
 {
-    std::ifstream stream{path, std::ios::binary | std::ios::ate};
-    VKE_ASSERT_MSG(stream.is_open(), "input file stream could not be opened: {}", path.string());
-
-    const std::ifstream::pos_type file_size = stream.tellg();
+    const fs::path actual_path = path.is_absolute() ? path : absolute(path);
+    std::ifstream stream{actual_path, std::ios::binary};
+    VKE_ASSERT_MSG(stream.is_open(), "input file stream could not be opened: {}", actual_path.string());
 
     std::vector<u8> bytes;
-    bytes.reserve(file_size);
+    {
+        const auto size = file_size(actual_path);
+        bytes.resize(size);
+    }
 
-    stream.seekg(0, std::ios::beg);
-    stream.read(reinterpret_cast<char*>(bytes.data()), file_size); // NOLINT
+    std::copy(
+      std::istreambuf_iterator<char>{stream},
+      std::istreambuf_iterator<char>{},
+      bytes.begin());
 
-    VKE_LOG(fs, verbose, "read {} bytes from {}", bytes.size(), path.string());
+    VKE_LOG(fs, verbose, "read {} bytes from {}", bytes.size(), actual_path.string());
     return bytes;
 }
 
 void write_file(const path& path, const std::span<const u8> data)
 {
-    std::ofstream stream{path, std::ios::binary};
-    VKE_ASSERT_MSG(stream.is_open(), "output file stream could not be opened: {}", path.string());
+    const fs::path actual_path = path.is_absolute() ? path : std::filesystem::current_path() / path;
+    std::ofstream stream{actual_path, std::ios::binary};
+    VKE_ASSERT_MSG(stream.is_open(), "output file stream could not be opened: {}", actual_path.string());
 
-    stream.write(reinterpret_cast<const char*>(data.data()), data.size()); // NOLINT
-    VKE_LOG(fs, verbose, "wrote {} bytes to {}", data.size(), path.string());
+    std::ranges::copy(data, std::ostreambuf_iterator<char>(stream));
+    VKE_LOG(fs, verbose, "wrote {} bytes to {}", data.size(), actual_path.string());
 }
 
 void write_file(const path& path, const std::vector<u8>& data)
