bd25a1a80f584a11b4d2de29dd5e8df46a7fa696
emrsmsrli <emrsmsrli1@gmail.com>
2023-02-02 23:52:28 +0300

    update vma-hpp

--- engine/src/renderer/vma_impl.cpp
+++ engine/src/renderer/vma_impl.cpp
@@ -5,5 +5,28 @@
  * Refer to the included LICENSE file.
  */
 
+#if _MSVC_LANG
+  #pragma warning(push)
+  #pragma warning(disable:4100) // unreferenced formal parameter
+  #pragma warning(disable:4127) // conditional expression is constant
+  #pragma warning(disable:4189) // local variable is initialized but not referenced
+  #pragma warning(disable:4324) // structure was padded due to alignment specifier
+#elif __clang__
+  #pragma clang diagnostic push
+  #pragma clang diagnostic ignored "-Wtautological-compare" // comparison of unsigned expression < 0 is always false
+  #pragma clang diagnostic ignored "-Wunused-private-field"
+  #pragma clang diagnostic ignored "-Wunused-parameter"
+  #pragma clang diagnostic ignored "-Wmissing-field-initializers"
+  #pragma clang diagnostic ignored "-Wnullability-completeness"
+  // todo disable gcc warnings
+#endif
+
 #define VMA_IMPLEMENTATION
-#include "renderer/vk_include.h"
+#include <vulkan_mem_alloc/vk_mem_alloc.h>
+
+#if _MSVC_LANG
+  #pragma warning(pop)
+#elif __clang__
+  #pragma clang diagnostic pop
+  // todo reenable gcc warnings
+#endif // PLATFORM
